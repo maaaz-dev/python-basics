de135cf26aadf0c424a2a67ec3cd083eebd9d374
Md Ma aaz Iftekhar <mdmaaaziftekhar@gmail.com>
2026-08-07 18:41:02 +0530

    Data Types Practice

---
+++ 02_data_types.py
@@ -0,0 +1,14 @@
+age = 20
+height = 5.11
+name = "Md Ma Aaz Iftekhar"
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Student:", is_student)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
