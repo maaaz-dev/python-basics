e23d5fd597d664afe37e90db644017d88806d53e
Md Ma aaz Iftekhar <mdmaaaziftekhar@gmail.com>
2026-08-09 12:47:08 +0530

    Operators Practice

---
+++ 03_operators.py
@@ -0,0 +1,10 @@
+a = 10
+b = 2
+
+print("addition :" , a + b)
+print("Subtraction :" , a - b)
+print("Multiplication :" , a * b)
+print("Division :" , a / b)
+print("Modulus :" , a % b)
+print("Floor Division :" , a // b)
+print("Power :" , a ** b)
